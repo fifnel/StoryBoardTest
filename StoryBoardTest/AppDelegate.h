0022c190177e1a1c1949c3554f345093f7089455
fifnel <fifnel@gmail.com>
2011-12-14 12:24:45 +0900

    Initial Commit

---
+++ StoryBoardTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryBoardTest
+//
+//  Created by fifnel on 2011/12/14.
+//  Copyright (c) 2011年 fifnel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoryBoardTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryBoardTest
+//
+//  Created by fifnel on 2011/12/14.
+//  Copyright (c) 2011年 fifnel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
